2289f8abb92b287e89588cc37f505a8307c3f1b8
4rrichard <102076693+4rrichard@users.noreply.github.com>
2025-11-03 11:58:05 +0100

    Ignore .env files and update Docker + Vite configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,10 @@
-import dotenv from "dotenv";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+import path from "path";
 
-const envFile = process.env.DOCKER ? "../.env.docker" : "../.env.local";
-dotenv.config({ path: envFile });
+const mode = process.env.DOCKER ? "docker" : "local";
+dotenv.config({ path: path.resolve(__dirname, `../.env.${mode}`) });
 
 export default defineConfig({
     plugins: [react()],
